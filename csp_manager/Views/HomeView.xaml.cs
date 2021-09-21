d0a1cb87107bd9cab64bd15af8d2905ea4aa2986
NthienMinh <nthienminh3001@gmail.com>
2021-09-21 15:20:32 +0700

    update

--- csp_manager/Views/HomeView.xaml.cs
+++ csp_manager/Views/HomeView.xaml.cs
@@ -22,15 +22,52 @@ namespace csp_manager.Views
     public partial class HomeView : UserControl, INotifyPropertyChanged
     {
         private Visibility isShowDialog;
+        private String icPlant;
+        private String icStatistics;
+        private String icSetting;
+        private String icHistory;
 
         public Visibility IsShowDialog
         {
             get => isShowDialog; set
             {
-                isShowDialog = value; 
+                isShowDialog = value;
                 OnPropertyChanged(nameof(IsShowDialog));
             }
         }
+
+        public string IcPlant
+        {
+            get => icPlant; set
+            {
+                icPlant = value;
+                OnPropertyChanged(nameof(IcPlant));
+            }
+        }
+        public string IcStatistics
+        {
+            get => icStatistics; set
+            {
+                icStatistics = value;
+                OnPropertyChanged(nameof(IcStatistics));
+            }
+        }
+        public string IcSetting
+        {
+            get => icSetting; set
+            {
+                icSetting = value;
+                OnPropertyChanged(nameof(IcSetting));
+            }
+        }
+        public string IcHistory
+        {
+            get => icHistory; set
+            {
+                icHistory = value;
+                OnPropertyChanged(nameof(IcHistory));
+            }
+        }       
 
         public HomeView()
         {
@@ -39,31 +76,71 @@ namespace csp_manager.Views
             homeFrame.Content = new AllListingPlantView();
             IsShowDialog = Visibility.Hidden;
             dialogFrame.Content = new AddItemView(this);
+            IcPlant = "pack://application:,,,/Res/Icons/ic_plant_selected.png";
+            IcStatistics = "pack://application:,,,/Res/Icons/ic_statistics.png";
+            IcSetting = "pack://application:,,,/Res/Icons/ic_setting.png";
+            IcHistory = "pack://application:,,,/Res/Icons/ic_history.png";
         }
 
         private void btnAllList_Click(object sender, RoutedEventArgs e)
         {
-            btnStatistic.Background = new SolidColorBrush();
-            btnSetting.Background = new SolidColorBrush();
             homeFrame.NavigationService.Navigate(new AllListingPlantView());
+            btnStatistic.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnSetting.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnHistory.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));           
+            btnLogout.Foreground = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            IcPlant = "pack://application:,,,/Res/Icons/ic_plant_selected.png";
+            IcStatistics = "pack://application:,,,/Res/Icons/ic_statistics.png";
+            IcSetting = "pack://application:,,,/Res/Icons/ic_setting.png";
+            IcHistory = "pack://application:,,,/Res/Icons/ic_history.png";
         }
+
         private void btnStatistic_Click(object sender, RoutedEventArgs e)
         {
-            btnAllList.Background = new SolidColorBrush();
-            btnSetting.Background = new SolidColorBrush();
             homeFrame.NavigationService.Navigate(new StatisticView());
+            btnAllList.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnSetting.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnHistory.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnLogout.Foreground = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            IcPlant = "pack://application:,,,/Res/Icons/ic_plant.png";
+            IcStatistics = "pack://application:,,,/Res/Icons/ic_statistics_selected.png";
+            IcSetting = "pack://application:,,,/Res/Icons/ic_setting.png";
+            IcHistory = "pack://application:,,,/Res/Icons/ic_history.png";
         }
 
         private void btnSetting_Click(object sender, RoutedEventArgs e)
         {
-            btnAllList.Background = new SolidColorBrush();
-            btnStatistic.Background = new SolidColorBrush();
             homeFrame.NavigationService.Navigate(new SettingView());
+            btnAllList.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnStatistic.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnHistory.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnLogout.Foreground = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            IcPlant = "pack://application:,,,/Res/Icons/ic_plant.png";
+            IcStatistics = "pack://application:,,,/Res/Icons/ic_statistics.png";
+            IcSetting = "pack://application:,,,/Res/Icons/ic_setting_selected.png";
+            IcHistory = "pack://application:,,,/Res/Icons/ic_history.png";
+        }
+
+        private void btnHistory_Click(object sender, RoutedEventArgs e)
+        {
+            homeFrame.NavigationService.Navigate(new SettingView());
+            btnAllList.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnStatistic.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnSetting.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            btnLogout.Foreground = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            IcPlant = "pack://application:,,,/Res/Icons/ic_plant.png";
+            IcStatistics = "pack://application:,,,/Res/Icons/ic_statistics.png";
+            IcSetting = "pack://application:,,,/Res/Icons/ic_setting.png";
+            IcHistory = "pack://application:,,,/Res/Icons/ic_history_selected.png";
         }
 
         private void btnLogout_Click(object sender, RoutedEventArgs e)
         {
             IsShowDialog = Visibility.Visible;
+            //btnAllList.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            //btnStatistic.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            //btnHistory.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
+            //btnSetting.Foreground = new SolidColorBrush(Color.FromRgb(20, 20, 20));
         }
 
         private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
@@ -76,5 +153,7 @@ namespace csp_manager.Views
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+
     }
 }
