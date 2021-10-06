9ba5c7feff10da26988ac3fc6f175a2c3778b49e
phatvphat <phatvphat@gmail.com>
2021-10-06 16:32:33 +0700

    thêm truy vấn db test

---
+++ csp_manager/DataQuery/QueryData.cs
@@ -0,0 +1,30 @@
+﻿using csp_manager.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csp_manager.DataQuery
+{
+    class PlantTypeDTO
+    {
+        public int pt_id { get; set; }
+        public string pt_name { get; set; }
+    }
+    class QueryData
+    {
+        public List<PlantTypeDTO> GetPlantType()
+        {
+            using (var dbContext = new CSPDbModel())
+            {
+                return (from pt in dbContext.plant_type
+                        select new PlantTypeDTO()
+                        {
+                            pt_id = pt.pt_id,
+                            pt_name = pt.pt_name
+                        }).ToList();
+            }
+        }
+    }
+}
